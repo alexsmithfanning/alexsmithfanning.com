5d350794bca7f4738acaec2ef9b5c82ea42fe402
Alex Smith-Fanning <alexsmithfanning@gmail.com>
2016-03-05 02:36:41 -0600

    Added a metric shit load of stuff to the website.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # alexsmithfanning.github.io
 Source code of my personal website
+
+## Attribution
+Special thanks to the [Materialize team](materializecss.com) for the awesome work they're doing! It made my life so much easier!
---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+/*
+ *  Copyright © 2016 Alex Smith-Fanning and others
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Used for generating the mobile fly-out menu
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+});
+
+// This is for the Twitter button
+! function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    p = /^http:/.test(d.location) ? "http" : "https";
+  if (!d.getElementById(id)) {
+    js = d.createElement(s);
+    js.id = id;
+    js.src = p + "://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+}(document, "script", "twitter-wjs");
