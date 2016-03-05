b5533d96dedeea0814a289b93350d471d8a30d52
Alex Smith-Fanning <alexsmithfanning@gmail.com>
2016-03-05 04:35:35 -0600

    Testing layout and icon on mobile navbar.

--- javascript/script.js
+++ javascript/script.js
@@ -32,3 +32,44 @@ $(document).ready(function() {
     fjs.parentNode.insertBefore(js, fjs);
   }
 }(document, "script", "twitter-wjs");
+
+// For Google+ button
+(function() {
+  var po = document.createElement('script');
+  po.type = 'text/javascript';
+  po.async = true;
+  po.src = 'https://apis.google.com/js/platform.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(po, s);
+})();
+
+// Code needed for ScrollFire to work
+var options = [{
+  selector: '.class',
+  offset: 200,
+  callback: 'globalFunction()'
+}, {
+  selector: '.other-class',
+  offset: 200,
+  callback: 'globalFunction()'
+}, ];
+Materialize.scrollFire(options);
+
+var options = [{
+  selector: '#staggered-test',
+  offset: 50,
+  callback: 'Materialize.toast("This is our ScrollFire Demo!", 1500 )'
+}, {
+  selector: '#staggered-test',
+  offset: 205,
+  callback: 'Materialize.toast("Please continue scrolling!", 1500 )'
+}, {
+  selector: '#staggered-test',
+  offset: 400,
+  callback: 'Materialize.showStaggeredList("#staggered-test")'
+}, {
+  selector: '#image-test',
+  offset: 500,
+  callback: 'Materialize.fadeInImage("#image-test")'
+}];
+Materialize.scrollFire(options);
