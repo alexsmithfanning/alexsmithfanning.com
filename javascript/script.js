bbd84c48e8c505c8a3a386bea2a55551d12b83ec
Alex Smith-Fanning <alexsmithfanning@gmail.com>
2016-03-31 12:27:05 -0500

    Inproved the position of the mobile nav button activator, recentered the profile image, used a different icon for social.html

--- javascript/script.js
+++ javascript/script.js
@@ -42,34 +42,3 @@ $(document).ready(function() {
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(po, s);
 })();
-
-// Code needed for ScrollFire to work
-var options = [{
-  selector: '.class',
-  offset: 200,
-  callback: 'globalFunction()'
-}, {
-  selector: '.other-class',
-  offset: 200,
-  callback: 'globalFunction()'
-}, ];
-Materialize.scrollFire(options);
-
-var options = [{
-  selector: '#staggered-test',
-  offset: 50,
-  callback: 'Materialize.toast("This is our ScrollFire Demo!", 1500 )'
-}, {
-  selector: '#staggered-test',
-  offset: 205,
-  callback: 'Materialize.toast("Please continue scrolling!", 1500 )'
-}, {
-  selector: '#staggered-test',
-  offset: 400,
-  callback: 'Materialize.showStaggeredList("#staggered-test")'
-}, {
-  selector: '#image-test',
-  offset: 500,
-  callback: 'Materialize.fadeInImage("#image-test")'
-}];
-Materialize.scrollFire(options);
