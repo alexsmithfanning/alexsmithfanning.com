b5d1d8e29127c5450d2b223184729b312c31ab5b
Alex Smith-Fanning <alexsmithfanning@gmail.com>
2016-03-31 20:36:50 -0500

    Removed the work file, redid the social media buttons, and added a contact page.

--- javascript/script.js
+++ javascript/script.js
@@ -22,23 +22,4 @@ $(document).ready(function() {
 });
 
 // This is for the Twitter button
-! function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    p = /^http:/.test(d.location) ? "http" : "https";
-  if (!d.getElementById(id)) {
-    js = d.createElement(s);
-    js.id = id;
-    js.src = p + "://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }
-}(document, "script", "twitter-wjs");
-
-// For Google+ button
-(function() {
-  var po = document.createElement('script');
-  po.type = 'text/javascript';
-  po.async = true;
-  po.src = 'https://apis.google.com/js/platform.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(po, s);
-})();
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
